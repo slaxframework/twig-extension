543dfe520abd4203699a3bf4616d281395bcef49
Eray AYDIN <erayaydin2016@gmail.com>
2016-06-20 13:53:44 +0300

    First initialize

---
+++ MenuRenderer.php
@@ -0,0 +1,23 @@
+<?php
+namespace SlaxFramework\TwigExtension;
+
+use \Slim;
+
+class MenuRenderer extends \Twig_Extension
+{
+    public function getName()
+    {
+        return 'menu_renderer';
+    }
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('render_menu', array($this, 'renderMenu'))
+        );
+    }
+    public function renderMenu($name, $tag = 'ul', $option)
+    {
+        $app = Slim::getInstance();
+        return $app->menu->render($name, $tag, $option);
+    }
+}
